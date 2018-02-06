b402fc64df17d6c9f568171a76b8d2cf1e9e8bcd
ZCforiOS <18855526615@163.com>
2018-02-06 15:30:44 +0800

    create folder

---
+++ OnChain/Models/UserModel.h
@@ -0,0 +1,13 @@
+//
+//  UserModel.h
+//  OnChain
+//
+//  Created by Zeus.Zhang on 2018/2/6.
+//  Copyright © 2018年 Zeus. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UserModel : NSObject
+
+@end
